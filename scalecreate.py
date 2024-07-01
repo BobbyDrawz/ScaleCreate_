457e6da02faa006cb5460f86f77c170bcf0cced0
BobbyDrawz_ <veersparekh@outlook.com>
2024-06-30 20:53:09 -0400

    removed goofy ahh offset

--- scalecreate.py
+++ scalecreate.py
@@ -25,7 +25,7 @@ track = MidiTrack()
 mid.tracks.append(track)
 
 # Add the chromatic scale notes to the track
-for i in range(range_notes + 1):
+for i in range(range_notes):
     note = start_midi + i
     track.append(Message('note_on', note=note, velocity=78, time=0))
     track.append(Message('note_off', note=note, velocity=78, time=note_length))
